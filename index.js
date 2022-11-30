c34f9ec1d6a9f95f769fd505e1dc0c9f3c4dfcdd
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-11-30 19:27:10 -0300

    feat: configurando os frameworks

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extend: false}))
+app.use(express.json())
+app.use(cors())
