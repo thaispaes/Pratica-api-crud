433ae6b5ec9edb5dd14326bf554579defe3b0d20
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-11-30 20:38:17 -0300

    feat: mudanças

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const routes = express.Router()
 
 let db = [
-    {'1': {Nome: 'Cliente 1', Idade: '20'}},
-    {'2': {Nome: 'Cliente 2', Idade: '20'}},
-    {'3': {Nome: 'Cliente 3', Idade: '20'}}
+    {'1': {Nome: 'Samantha', Idade: '20'}},
+    {'2': {Nome: 'Thais', Idade: '40'}},
+    {'3': {Nome: 'Carlos', Idade: '18'}}
 ]
 
 
